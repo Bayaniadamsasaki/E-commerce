0513211fbbd945462257995e8d9ac57a3f1b1742
Bayaniadamsasaki <bayaniadamsasaki@gmail.com>
2023-07-07 03:08:22 +0800

    Update Navbar/Images

--- script/script.js
+++ script/script.js
@@ -11,10 +11,24 @@ window.onscroll = () => {
   navbar.classList.remove('active');
 }
 
-document.querySelector('#search-icon').onclick = () => {
+document.querySelector('#bars').onclick = () => {
   document.querySelector('#search-form').classList.toggle('active');
 }
 
 document.querySelector('#close').onclick = () => {
   document.querySelector('#search-form').classList.remove('active');
 }
+
+var swiper = new Swiper(".home-slider", {
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 4500,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      loop:true,
+    });
